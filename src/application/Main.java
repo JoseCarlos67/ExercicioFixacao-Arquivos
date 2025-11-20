ced5e67d066af133566909e847b38b4735ab8387
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-11-20 17:41:37 -0300

    Utilizando FileReader e BufferedReader

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,37 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
     File file = new File("src/files/in.csv");
+    BufferedReader bufferedReader = null;
+    FileReader fileReader = null;
     Scanner scanner = null;
     try {
-      scanner = new Scanner(file);
-      while (scanner.hasNextLine())
-        System.out.println(scanner.nextLine());
+      fileReader = new FileReader("src/files/in.csv");
+      bufferedReader = new BufferedReader(fileReader);
+
+      String line = bufferedReader.readLine();
+      while (line != null) {
+        System.out.println(line);
+        line = bufferedReader.readLine();
+      }
     } catch (IOException e) {
       System.out.println("Error: " + e.getMessage());
     } finally {
-      if (scanner != null)
-        scanner.close();
+      try {
+        if (bufferedReader != null)
+          bufferedReader.close();
+        if (fileReader != null)
+          fileReader.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
