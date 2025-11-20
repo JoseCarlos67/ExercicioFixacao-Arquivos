533ad531e58b383226218721d235063f4f4ba6ea
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-11-20 17:31:24 -0300

    Lendo o arquivo

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,22 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    File file = new File("src/files/in.csv");
+    Scanner scanner = null;
+    try {
+      scanner = new Scanner(file);
+      while (scanner.hasNextLine())
+        System.out.println(scanner.nextLine());
+    } catch (IOException e) {
+      System.out.println("Error: " + e.getMessage());
+    } finally {
+      if (scanner != null)
+        scanner.close();
+    }
   }
 }
